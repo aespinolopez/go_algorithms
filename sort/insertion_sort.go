a5de941614a086fa4b488222e82bd30fea559a43
aespinolopez <aespino1290@gmail.com>
2019-10-26 03:00:06 +0200

    insertion sort ut

--- sort/insertion_sort.go
+++ sort/insertion_sort.go
@@ -1,7 +1,7 @@
 package sort
 
 func InsertionSort(arr []int) {
-	for j := 2; j < len(arr); j++ {
+	for j := 1; j < len(arr); j++ {
 		var key = arr[j]
 		var i = j - 1
 		for i >= 0 && arr[i] > key {
